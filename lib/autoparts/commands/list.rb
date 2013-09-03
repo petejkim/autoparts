6f2d1491e90fc025f7d5a7fcc240f6ffcc5925ca
Peter Jihoon Kim <raingrove@gmail.com>
2013-09-03 13:18:56 +0800

    fixed a bug that caused "no installed package found" message to show always

--- lib/autoparts/commands/list.rb
+++ lib/autoparts/commands/list.rb
@@ -3,7 +3,7 @@ module Autoparts
     class List
       def initialize(args, options)
         installed = Autoparts::Package.installed
-        puts "parts: no installed package found"
+        puts "parts: no installed package found" if installed.length == 0
         installed.each_pair do |package, versions|
           puts "#{package} (#{versions.join ', '})"
         end
