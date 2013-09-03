7edbf0651b58bdd38c1f639a213008032479daee
Peter Jihoon Kim <raingrove@gmail.com>
2013-09-03 12:36:15 +0800

    chdir into project root before attempting to get git hash

--- lib/autoparts/commands/help.rb
+++ lib/autoparts/commands/help.rb
@@ -4,26 +4,28 @@ module Autoparts
   module Commands
     class Help
       def initialize(args, options)
-        git_hash = `git describe --always`.strip
-        version = "Autoparts #{Autoparts::VERSION}-#{git_hash}"
+        Dir.chdir(PROJECT_ROOT) do
+          git_hash = `git describe --always`.strip
+          version = "Autoparts #{Autoparts::VERSION}-#{git_hash}"
 
-        if options.include?('-v') || options.include?('--version')
-          puts version
-        else
-          puts <<-EOS.unindent
-            #{version} - A Package Manager for Nitrous.IO
+          if options.include?('-v') || options.include?('--version')
+            puts version
+          else
+            puts <<-EOS.unindent
+              #{version} - A Package Manager for Nitrous.IO
 
-            Usage: parts COMMAND [ARGS...]
+              Usage: parts COMMAND [ARGS...]
 
-            Some useful commands are:
-              parts install PACKAGE...      # Install one or many packages
-              parts uninstall PACKAGE...    # Uninstall one or many packages
-              parts list                    # List all installed packages
-              parts start PACKAGE...        # Start one or many services provided by packages
-              parts stop PACKAGE...         # Stop one or many services provided by packages
-              parts restart PACKAGE...      # Restart one or many services provided by packages
-              parts update                  # Update Autoparts
-          EOS
+              Some useful commands are:
+                parts install PACKAGE...      # Install one or many packages
+                parts uninstall PACKAGE...    # Uninstall one or many packages
+                parts list                    # List all installed packages
+                parts start PACKAGE...        # Start one or many services provided by packages
+                parts stop PACKAGE...         # Stop one or many services provided by packages
+                parts restart PACKAGE...      # Restart one or many services provided by packages
+                parts update                  # Update Autoparts
+            EOS
+          end
         end
       end
     end
