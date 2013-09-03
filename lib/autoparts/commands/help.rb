d3fefe86225c14dea32dcf8f4952ac801d03e6ea
Peter Jihoon Kim <raingrove@gmail.com>
2013-09-03 12:01:16 +0800

    added '-v', '--version'. show git hash in parts help.

--- lib/autoparts/commands/help.rb
+++ lib/autoparts/commands/help.rb
@@ -4,20 +4,27 @@ module Autoparts
   module Commands
     class Help
       def initialize(args, options)
-        puts <<-EOS.unindent
-          Autoparts #{Autoparts::VERSION} A Package Manager for Nitrous.IO
+        git_hash = `git describe --always`.strip
+        version = "Autoparts #{Autoparts::VERSION}-#{git_hash}"
 
-          Usage: parts COMMAND [ARGS...]
+        if options.include?('-v') || options.include?('--version')
+          puts version
+        else
+          puts <<-EOS.unindent
+            #{version} - A Package Manager for Nitrous.IO
 
-          Some useful commands are:
-            parts install PACKAGE...      # Install one or many packages
-            parts uninstall PACKAGE...    # Uninstall one or many packages
-            parts list                    # List all installed packages
-            parts start PACKAGE...        # Start one or many services provided by packages
-            parts stop PACKAGE...         # Stop one or many services provided by packages
-            parts restart PACKAGE...      # Restart one or many services provided by packages
-            parts update                  # Update Autoparts
-        EOS
+            Usage: parts COMMAND [ARGS...]
+
+            Some useful commands are:
+              parts install PACKAGE...      # Install one or many packages
+              parts uninstall PACKAGE...    # Uninstall one or many packages
+              parts list                    # List all installed packages
+              parts start PACKAGE...        # Start one or many services provided by packages
+              parts stop PACKAGE...         # Stop one or many services provided by packages
+              parts restart PACKAGE...      # Restart one or many services provided by packages
+              parts update                  # Update Autoparts
+          EOS
+        end
       end
     end
   end
