ae81fa5091efaac2d2bb333b6f8fefcb956fc7c4
Peter Jihoon Kim <raingrove@gmail.com>
2013-09-05 16:12:15 +0800

    refactored redis package with running? method

--- lib/autoparts/packages/redis.rb
+++ lib/autoparts/packages/redis.rb
@@ -72,33 +72,35 @@ module Autoparts
         Path.var + 'log' + 'redis'
       end
 
+      def read_redis_pid_file
+        @redis_pid ||= File.read(redis_pid_file_path).strip
+      end
+
       def start
-        # redis-cli returns 0 even if AUTH 0 command fails as long as connection is made
-        if system "#{bin_path}/redis-cli", 'AUTH', '0', out: '/dev/null', err: '/dev/null'
-          raise StartFailedError.new "#{name} is already running."
-        end
+        raise StartFailedError.new "#{name} is already running." if running?
         execute redis_server_path, redis_conf_path
       end
 
       def stop
-        if redis_pid_file_path.exist?
-          pid = File.read(redis_pid_file_path).strip
-          # check if pid actually belongs to redis process
-          if pid.length > 0 && `ps -o cmd= #{pid}`.include?(redis_server_path.basename.to_s)
-            execute 'kill', pid
-            # wait until process is killed
-            sleep 0.2 while system 'kill', '-0', pid, out: '/dev/null', err: '/dev/null'
-            # redis should remove pid file on its own, but delete if it still exists
-            redis_pid_file_path.unlink if redis_pid_file_path.exist?
-            return
-          end
-          # pid belongs to some other process. just delete pid file.
-          redis_pid_file_path.unlink
+        if running?
+          pid = read_redis_pid_file
+          execute 'kill', pid
+          # wait until process is killed
+          sleep 0.2 while system 'kill', '-0', pid, out: '/dev/null', err: '/dev/null'
+          return
         end
+        redis_pid_file_path.unlink if redis_pid_file_path.exist?
         raise StopFailedError.new "#{name} does not seem to be running."
       end
 
-      def status
+      def running?
+        if redis_pid_file_path.exist?
+          pid = read_redis_pid_file
+          if pid.length > 0 && `ps -o cmd= #{pid}`.include?(redis_server_path.basename.to_s)
+            return true
+          end
+        end
+        false
       end
 
       def tips
