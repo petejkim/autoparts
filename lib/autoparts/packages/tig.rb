ae057b10137639f35eae24766bc7c1e5ff4206f8
arunthampi <arun.thampi@gmail.com>
2014-01-21 20:59:40 +0000

    Remove binary_url/binary_sha1 from tig

--- lib/autoparts/packages/tig.rb
+++ lib/autoparts/packages/tig.rb
@@ -7,8 +7,6 @@ module Autoparts
       source_url 'http://jonas.nitro.dk/tig/releases/tig-1.2.1.tar.gz'
       source_sha1 '5755bae7342debf94ef33973e0eaff6207e623dc'
       source_filetype 'tar.gz'
-      binary_url 'https://nitrousio-autoparts-use1.s3.amazonaws.com/tig-1.2.1-binary.tar.gz'
-      binary_sha1 '124ca210166fe03b9a4b7d134d3ecf9f7a25a63c'
 
       def compile
         Dir.chdir(name_with_version) do
