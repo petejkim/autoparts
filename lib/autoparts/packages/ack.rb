32a59142535b2f430e1ca81834f4eae5ca5f697d
arunthampi <arun.thampi@gmail.com>
2014-01-23 07:46:01 +0000

    Upgrade ack version

--- lib/autoparts/packages/ack.rb
+++ lib/autoparts/packages/ack.rb
@@ -2,10 +2,10 @@ module Autoparts
   module Packages
     class Ack < Package
       name 'ack'
-      version '2.08'
+      version '2.12'
       description 'Ack: A tool like grep, optimized for programmers'
-      source_url 'http://beyondgrep.com/ack-2.08-single-file'
-      source_sha1 'c0fa19ba31ecc1afe186c5a061c1361ae2a258f3'
+      source_url 'http://beyondgrep.com/ack-2.12-single-file'
+      source_sha1 '667b5f2dd83143848a5bfa47f7ba848cbe556e93'
       source_filetype 'pl'
 
       def install
