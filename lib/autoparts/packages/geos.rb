20240aa0e77386da358fba0fa02d9bdcad036e0e
arunthampi <arun.thampi@gmail.com>
2014-02-06 01:50:09 +0000

    Remove extra space in description of Geos

--- lib/autoparts/packages/geos.rb
+++ lib/autoparts/packages/geos.rb
@@ -3,7 +3,7 @@ module Autoparts
     class Geos < Package
       name 'geos'
       version '3.4.2'
-      description 'GEOS: C++ port of the  Java Topology Suite (JTS)'
+      description 'GEOS: C++ port of the Java Topology Suite (JTS)'
       source_url 'http://download.osgeo.org/geos/geos-3.4.2.tar.bz2'
       source_sha1 'b8aceab04dd09f4113864f2d12015231bb318e9a'
       source_filetype 'tar.bz2'
