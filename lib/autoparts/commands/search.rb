19fa89beaabd074a0501c632848821f1805df3f5
Maxim Kraev <maxim.kraev@gmail.com>
2014-02-14 10:30:10 +0400

    better search output formatting

--- lib/autoparts/commands/search.rb
+++ lib/autoparts/commands/search.rb
@@ -21,16 +21,44 @@ module Autoparts
             list["#{name}".bold.magenta + " (#{package.version})".green] = package.description
           end
           unless list.empty?
+            colors_length = ''.bold.magenta.green.length
             ljust_length = list.keys.map(&:length).max + 1
+            columns = `tput cols`.to_i
+            format = "%-#{ljust_length}s %s\n"
             list.each_pair do |name, description|
-              print name.ljust(ljust_length)
-              print description if description
-              puts
+              if (description.length + ljust_length < columns)
+                printf format, name, description
+              else
+                descriptions = split_description description, columns - ljust_length + colors_length - 4
+                descriptions.each do |description|
+                  printf format, name, description
+                  name = ' '.bold.magenta.green
+                end
+              end
             end
           end
         else
           abort "parts: no package found for \"#{args[0]}\""
         end
+      end
+
+      def split_description(description, length)
+        words = description.strip.split(' ');
+        result = []
+        line = ""
+        words.each do |word|
+          if line.length + word.length > length
+            result.push(line)
+            line = ""
+          end
+          if line.length > 0
+            line = [line, word].join(' ')
+          else
+            line = word
+          end
+        end
+        result.push(line) if line.length > 0
+        result
       end
     end
   end
