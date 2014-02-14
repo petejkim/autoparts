2147017cdfb007d3297dae7e5a24845077d643d5
Maxim Kraev <maxim.kraev@gmail.com>
2014-02-14 10:41:12 +0400

    php5 modules: oath

---
+++ lib/autoparts/packages/php5-oauth.rb
@@ -0,0 +1,38 @@
+require File.join(File.dirname(__FILE__), 'php5ext')
+
+module Autoparts
+  module Packages
+    class Php5ExtOauth < Php5Ext
+      name 'php5-oauth'
+
+      def version
+        '1.2.3'
+      end
+
+      description 'OAuth module for php5'
+
+      def source_url
+        'http://pecl.php.net/get/oauth-1.2.3.tgz'
+      end
+
+      def source_sha1
+        'e2a42961c8134746fc0cd8ef9bd433f760b94975'
+      end
+
+      def source_filetype
+        'tgz'
+      end
+
+      depends_on 'php5'
+
+      def php_extension_name
+        "oauth"
+      end
+
+      def php_extension_dir
+        "oauth-1.2.3"
+      end
+
+    end
+  end
+end
