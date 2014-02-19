b4013fcc9ac176983e5db313d2bfacd05b14cc03
RJ <ctrlaltreboot@gmail.com>
2014-02-19 09:26:22 +0000

    Add ruby 1.9

--- lib/autoparts/packages/ruby19.rb
+++ lib/autoparts/packages/ruby19.rb
@@ -1,9 +1,9 @@
 module Autoparts
   module Packages
-    class Ruby20 < Package
-      name 'ruby20'
-      version '2.0.0-p353'
-      description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
+    class Ruby19 < Package
+      name 'ruby19'
+      version '1.9.3-p484'
+      description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity. 1.9'
       source_filetype 'command'
 
       depends_on 'rvm'
@@ -11,7 +11,7 @@ module Autoparts
       def install
         prefix_path.mkpath
         execute 'touch', prefix_path + 'INSTALLED_BY_AUTOPARTS' # So that Autoparts thinks the package is installed.
-        execute "/bin/bash -c 'source #{rvm_shims} && rvm install #{version} && rvm use --default #{version}'"
+        execute "/bin/bash -c 'source #{rvm_shims} && rvm install #{version} && rvm --default use #{version}'"
       end
 
       def post_uninstall
