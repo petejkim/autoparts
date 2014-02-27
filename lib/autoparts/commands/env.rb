8562d351373761c3dec1d411c3397f0a74951a5e
Tim Goh <tim@nitrous.io>
2014-02-27 13:51:12 +0800

    Add parts env command

---
+++ lib/autoparts/commands/env.rb
@@ -0,0 +1,27 @@
+module Autoparts
+  module Commands
+    class Env
+      EXPORTS = {
+        'AUTOPARTS_ROOT'             => Path.root,
+        'PATH'                       => '$AUTOPARTS_ROOT/bin:$AUTOPARTS_ROOT/sbin:$PATH',
+        'LD_LIBRARY_PATH'            => '$AUTOPARTS_ROOT/lib:/usr/local/lib:/usr/lib:$LD_LIBRARY_PATH',
+        'LIBRARY_PATH'               => '$AUTOPARTS_ROOT/lib:/usr/local/lib:/usr/lib:$LIBRARY_PATH',
+        'DYLD_FALLBACK_LIBRARY_PATH' => '$AUTOPARTS_ROOT/lib',
+        'C_INCLUDE_PATH'             => '$AUTOPARTS_ROOT/include',
+        'CPLUS_INCLUDE_PATH'         => '$AUTOPARTS_ROOT/include',
+        'OBJC_INCLUDE_PATH'          => '$AUTOPARTS_ROOT/include',
+        'MAN_PATH'                   => '$AUTOPARTS_ROOT/share/man:/usr/local/share/man:/usr/share/man:$MAN_PATH'
+      }.freeze
+
+      def initialize(args, options)
+        Env.print_exports
+      end
+
+      def self.print_exports
+        EXPORTS.each do |envvar, value|
+          puts %Q(export #{envvar}="#{value}")
+        end
+      end
+    end
+  end
+end
