8bda02b62f353afd032e215faf7467a05a80ce30
Marc Seiler <mseiler@gmail.com>
2014-02-26 21:57:40 -0500

    forgot the checksum

--- lib/autoparts/packages/googleappengine.rb
+++ lib/autoparts/packages/googleappengine.rb
@@ -5,7 +5,7 @@ module Autoparts
       version '1.9.0'
       description 'Google App Engine Python/PHP: A CLI for managing Google App Engine cloud services for Python and PHP'
       source_url 'https://commondatastorage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.0.zip'
-      source_sha1 '6a5a79a81bf0f1fccf5b56dac41be6174888983e'
+      source_sha1 '923a7f99e058d93408d3940e0307bdf2769b7480'
       source_filetype 'zip'
 
       def install
