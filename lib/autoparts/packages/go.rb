22284f43ff5dc0d089fae3e242ab1620acc4122a
arunthampi <arun.thampi@gmail.com>
2014-03-03 16:11:06 +0000

    Add Go Package

---
+++ lib/autoparts/packages/go.rb
@@ -0,0 +1,44 @@
+# Copyright (c) 2013, Irrational Industries Inc. (Nitrous.IO)
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+module Autoparts
+  module Packages
+    class Go < Package
+      name 'go'
+      version '1.2.1'
+      description 'Go: An open source programming language that makes it easy to build simple, reliable, and efficient software.'
+      source_url 'https://go.googlecode.com/files/go1.2.1.linux-amd64.tar.gz'
+      source_sha1 '7605f577ff6ac2d608a3a4e829b255ae2ebc8dcf'
+      source_filetype 'tar.gz'
+
+      def install
+        Dir.chdir("go") do
+          prefix_path.mkpath
+          execute 'cp', '-R', '.', prefix_path
+        end
+      end
+    end
+  end
+end
+
+
