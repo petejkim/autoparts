59a8f8def943920300a727b25aaa2c231b3ce836
Chris Schneider <chris@christopher-schneider.com>
2014-03-03 16:12:55 +0000

    Add dependency from cabal_install to ghc

--- lib/autoparts/packages/cabal_install.rb
+++ lib/autoparts/packages/cabal_install.rb
@@ -7,6 +7,8 @@ module Autoparts
       source_url 'http://www.haskell.org/cabal/release/cabal-install-1.18.0.2/cabal-install-1.18.0.2.tar.gz'
       source_sha1 '2d1f7a48d17b1e02a1e67584a889b2ff4176a773'
       source_filetype 'tar.gz'
+
+      depends_on 'ghc'
       
       def compile
         Dir.chdir('cabal-install-1.18.0.2') do
