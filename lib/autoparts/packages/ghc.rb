a25eb34592651cf14f916d3c761f0f35ca05d02f
Lakshan Perera <lakshan@laktek.com>
2014-03-04 02:12:18 +0000

    A better description for GHC package.

--- lib/autoparts/packages/ghc.rb
+++ lib/autoparts/packages/ghc.rb
@@ -26,7 +26,7 @@ module Autoparts
     class Ghc < Package
       name 'ghc'
       version '7.6.3'
-      description 'GHC'
+      description 'GHC: a state-of-the-art, open source, compiler and interactive environment for the functional language Haskell.'
       source_url 'http://www.haskell.org/ghc/dist/7.6.3/ghc-7.6.3-x86_64-unknown-linux.tar.bz2'
       source_sha1 '46ec3f3352ff57fba0dcbc8d9c20f7bcb6924b77'
       source_filetype 'tar.bz2'
