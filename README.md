d52b50416c0a73c7faa21226f682b4550343835c
Peter Jihoon Kim <raingrove@gmail.com>
2014-03-05 02:19:16 +0000

    +copyright

--- README.md
+++ README.md
@@ -38,5 +38,5 @@ See `parts help`.
   `~/.parts/var/log/postgresql.log`).
 
 - - -
-Copyright (c) 2013 Irrational Industries Inc.
-This software is licensed under the BSD 2-Clause license.
+Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
+This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
--- lib/autoparts/packages/erlang.rb
+++ lib/autoparts/packages/erlang.rb
@@ -1,25 +1,5 @@
-# Copyright (c) 2013, Irrational Industries Inc. (Nitrous.IO)
-# # All rights reserved.
-#
-# # Redistribution and use in source and binary forms, with or without modification,
-# # are permitted provided that the following conditions are met:
-#
-# # 1. Redistributions of source code must retain the above copyright notice, this
-# #    list of conditions and the following disclaimer.
-# # 2. Redistributions in binary form must reproduce the above copyright notice,
-# #    this list of conditions and the following disclaimer in the documentation
-# #    and/or other materials provided with the distribution.
-#
-# # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# # DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-# # ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# # ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
+# This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
 
 module Autoparts
   module Packages
