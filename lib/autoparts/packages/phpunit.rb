a9920118b9cb74ff0b4f5a66054e5d91feefe1bc
Maxim Kraev <maxim.kraev@gmail.com>
2014-03-08 20:56:38 +0400

    typo fix

--- lib/autoparts/packages/phpunit.rb
+++ lib/autoparts/packages/phpunit.rb
@@ -1,9 +1,9 @@
 module Autoparts
   module Packages
-    class PhpUnit < Package
+    class Phpunit < Package
       name 'phpunit'
       version '3.7.32'
-      description 'PHPUnit is a programmer-oriented testing framework for PHP.'
+      description 'PHPUnit: A programmer-oriented testing framework for PHP.'
       source_url 'https://phar.phpunit.de/phpunit.phar'
       source_sha1 'cf74cb5abe92190c6e9aa472124e28ae5d5e49ca'
       source_filetype 'php'
@@ -12,11 +12,11 @@ module Autoparts
 
       def install
         bin_path.mkpath
-        execute 'mv', archive_filename, phpinut_executable_path
-        execute 'chmod', '0755', phpinut_executable_path
+        execute 'mv', archive_filename, phpunit_executable_path
+        execute 'chmod', '0755', phpunit_executable_path
       end
 
-      def phpinut_executable_path
+      def phpunit_executable_path
         bin_path + 'phpunit'
       end
 
