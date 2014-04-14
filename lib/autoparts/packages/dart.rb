277ddba98e39ce66861d6fd2a754071f7d48882b
Cheah Chu Yeow <chuyeow@gmail.com>
2014-04-14 14:38:55 +0800

    Use a fixed URL for Dart 1.3.0 release.

--- lib/autoparts/packages/dart.rb
+++ lib/autoparts/packages/dart.rb
@@ -2,13 +2,13 @@ module Autoparts
   module Packages
     class Dart < Package
       name 'dart'
-      version '1.2.0'
+      version '1.3.0'
 
-      description "Dart: new platform for scalable web app engineering."
-      category Category::PROGRAMMING_LANGUAGES 
+      description 'Dart: new platform for scalable web app engineering'
+      category Category::PROGRAMMING_LANGUAGES
 
-      source_url 'http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip'
-      source_sha1 '542600aa55653c2ee584d7358b32efee1f25c541'
+      source_url 'http://storage.googleapis.com/dart-archive/channels/stable/release/34825/sdk/dartsdk-linux-x64-release.zip'
+      source_sha1 '17a023802141092b42edb6f4c1db1b909fa79e36'
       source_filetype 'zip'
 
       def install
