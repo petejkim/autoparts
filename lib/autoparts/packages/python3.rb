e8550977fa072a40a25ad518c0d1e5b6211fa4c6
Daniel, Dao Quang Minh <dqminh89@gmail.com>
2014-05-21 04:00:46 -0400

    bump python3 version to 3.4.1

--- lib/autoparts/packages/python3.rb
+++ lib/autoparts/packages/python3.rb
@@ -7,10 +7,10 @@ module Autoparts
   module Packages
     class Python3 < Python2
       name 'python3'
-      version '3.4.0'
+      version '3.4.1'
       description 'Python 3: Next generation of the most friendly Programming Language'
-      source_url 'http://www.python.org/ftp/python/3.4.0/Python-3.4.0.tgz'
-      source_sha1 'bb5125d1c437caa5a62e0a3d0fee298e91196d6f'
+      source_url 'https://www.python.org/ftp/python/3.4.1/Python-3.4.1.tgz'
+      source_sha1 'e8c1bd575a6ccc2a75f79d9d094a6a29d3802f5d'
       source_filetype 'tgz'
       category Category::PROGRAMMING_LANGUAGES
 
