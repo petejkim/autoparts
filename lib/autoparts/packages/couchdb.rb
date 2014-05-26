700a884edf85f74e8220b5bf44ac25175cecc109
codelion <asankhaya@yahoo.com>
2014-05-26 20:36:02 +0000

    added auto part for couch db

--- lib/autoparts/packages/couchdb.rb
+++ lib/autoparts/packages/couchdb.rb
@@ -1,68 +1,31 @@
-# This package (aka part) is an example which will help guide you build a new
-# Autoparts package.
-
-# In order to create a new package you will need to provide the following
-# information when creating a pull request:
-
-# * Package Name
-# * Version
-# * Description
-# * Category (A list of categories can be found within [lib/autoparts/category.rb](https://github.com/nitrous-io/autoparts/blob/master/lib/autoparts/category.rb))
-# * Source URL (official release source which package will always be located at)
-# * Source filetype (the extension of the source file)
-# * SHA-1 hash of the source package (You can run `sha1sum` against the source file or [generate](http://hash.online-convert.com/sha1-generator) a SHA-1 hash with the source file)
-# * Dependencies (if any)
-# * Compile / Installation commands
-
-# Let's take a look at an existing part, MySQL ( https://github.com/action-io/autoparts/blob/master/lib/autoparts/packages/mysql.rb ):
-
-# Naming - Every package should be in the `Autoparts::Packages` module. Give it
-# a name and subclass `Package`.
+# Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
+# This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
 
 module Autoparts
   module Packages
     class CouchDB < Package
-      name 'couchdb' # This should be the name of the part in all lowercase letters.
-      version '1.5.1' # Version of the tool being installed.
+      name 'couchdb' 
+      version '1.5.1'
 
-      # This description should contain the human readable package name
-      # (e.g. 'MySQL'), followed by a colon and then description.
       description "CouchDB: A Database for the Web"
 
-      # Include a category for your part. A list of categories can be found at
-      # https://github.com/nitrous-io/autoparts/blob/master/lib/autoparts/category.rb.
       category Category::DATA_STORES
 
-      # The url of the source archive.
       source_url 'http://www.apache.org/dist/couchdb/source/1.5.1/apache-couchdb-1.5.1.tar.gz'
 
-      # The sha1 hash. If it is not provided by the host, download the
-      # archive and run `sha1sum filename` to obtain this.
       source_sha1 '5340c79f8f9e11742b723f92e2251d4d59b8247c'
 
-      # source_filetype is used to determine how to extract the package.
-      # Currently, variations of tar and zip are supported. Other filetypes are
-      # simply copied over without extraction.
       source_filetype 'tar.gz'
 
-      ## Dependencies
-      #
-      # Include any dependencies for this part.
-      # Any dependencies will be installed before proceeding.
-      # If any dependencies don't already exist as an Autoparts package, you'll
-      # need to add them as a separate package.
-      depends_on 'erlang' # Not actually needed for MySQL, but added as an example.
       depends_on 'spidermonkey'
+      depends_on 'erlangr16'
       
-      ## Installation
-      #
-      # At this step the archive from the `source_url` is downloaded, and the
-      # sha1 hash is checked. Autoparts creates a temporary directory where the
-      # archive is unpacked.
       def compile
         Dir.chdir('apache-couchdb-1.5.1') do
           args = [
             "--prefix=#{prefix_path}",
+            "--with-js-include=#{get_dependency("spidermonkey").include_path}/js" ,
+            "--with-js-lib=#{get_dependency("spidermonkey").lib_path}"
           ]
 
           execute './configure', *args
@@ -76,26 +39,13 @@ module Autoparts
         end
       end
 
-      def couchdb_server_path
-        bin_path + 'couchdb'
+      def couchdb_conf_path
+        prefix_path + 'etc/couchdb/default.ini'
       end
       
-      ## Starting and Stopping the part
-      #
-      # Some parts run as a service. If your part needs to run in the background
-      # then you will need to implement start and stop commands.
-      def start
-        execute couchdb_server_path, 'start'
-      end
-
-      def stop
-        execute couchdb_server_path, 'stop'
-      end
-
-      # If using start and stop commands, include a 'running?' method.
-      def running?
-        !!system(couchdb_server_path.to_s, 'status', out: '/dev/null', err: '/dev/null')
-      end
+      def post_install
+        execute 'sed', '-i', "s|bind_address = 127.0.0.1|bind_address = 0.0.0.0|g", couchdb_conf_path
+      end 
       
     end
   end
--- lib/autoparts/packages/spidermonkey.rb
+++ lib/autoparts/packages/spidermonkey.rb
@@ -1,61 +1,24 @@
-
-
-# * Package Name
-# * Version
-# * Description
-# * Category (A list of categories can be found within [lib/autoparts/category.rb](https://github.com/nitrous-io/autoparts/blob/master/lib/autoparts/category.rb))
-# * Source URL (official release source which package will always be located at)
-# * Source filetype (the extension of the source file)
-# * SHA-1 hash of the source package (You can run `sha1sum` against the source file or [generate](http://hash.online-convert.com/sha1-generator) a SHA-1 hash with the source file)
-# * Dependencies (if any)
-# * Compile / Installation commands
-
-# Let's take a look at an existing part, MySQL ( https://github.com/action-io/autoparts/blob/master/lib/autoparts/packages/mysql.rb ):
-
-# Naming - Every package should be in the `Autoparts::Packages` module. Give it
-# a name and subclass `Package`.
+# Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
+# This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
 
 module Autoparts
   module Packages
     class SpiderMonkey < Package
-      name 'spidermonkey' # This should be the name of the part in all lowercase letters.
-      version '24.2.0' # Version of the tool being installed.
+      name 'spidermonkey'
+      version '1.8.5'
 
-      # This description should contain the human readable package name
-      # (e.g. 'MySQL'), followed by a colon and then description.
       description "SpiderMonkey: Mozilla's JavaScript engine written in C/C++"
 
-      # Include a category for your part. A list of categories can be found at
-      # https://github.com/nitrous-io/autoparts/blob/master/lib/autoparts/category.rb.
       category Category::LIBRARIES
 
-      # The url of the source archive.
-      source_url 'https://ftp.mozilla.org/pub/mozilla.org/js/mozjs-24.2.0.tar.bz2'
+      source_url 'http://ftp.mozilla.org/pub/mozilla.org/js/js185-1.0.0.tar.gz'
 
-      # The sha1 hash. If it is not provided by the host, download the
-      # archive and run `sha1sum filename` to obtain this.
-      source_sha1 'ce779081cc11bd0c871c6f303fc4a0091cf4fe66'
+      source_sha1 '52a01449c48d7a117b35f213d3e4263578d846d6'
 
-      # source_filetype is used to determine how to extract the package.
-      # Currently, variations of tar and zip are supported. Other filetypes are
-      # simply copied over without extraction.
-      source_filetype 'tar.bz2'
+      source_filetype 'tar.gz'
 
-      ## Dependencies
-      #
-      # Include any dependencies for this part.
-      # Any dependencies will be installed before proceeding.
-      # If any dependencies don't already exist as an Autoparts package, you'll
-      # need to add them as a separate package.
-      #depends_on 'erlang' # Not actually needed for MySQL, but added as an example.
-
-      ## Installation
-      #
-      # At this step the archive from the `source_url` is downloaded, and the
-      # sha1 hash is checked. Autoparts creates a temporary directory where the
-      # archive is unpacked.
       def compile
-        Dir.chdir('mozjs-24.2.0/js/src') do
+        Dir.chdir('js-1.8.5/js/src') do
           args = [
             "--prefix=#{prefix_path}"
           ]
@@ -66,12 +29,11 @@ module Autoparts
       end
 
       def install
-        Dir.chdir('mozjs-24.2.0/js/src') do
+        Dir.chdir('js-1.8.5/js/src') do
           execute 'make', 'install'
         end
       end
-
-
+      
     end
   end
 end
