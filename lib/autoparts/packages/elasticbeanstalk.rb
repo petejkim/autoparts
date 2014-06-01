2832e2d2045ce73925481319218aa80ed2801201
codelion <asankhaya@yahoo.com>
2014-06-01 02:31:32 +0000

    Added Elasticbeanstalk package

---
+++ lib/autoparts/packages/elasticbeanstalk.rb
@@ -0,0 +1,43 @@
+# Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
+# This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
+
+module Autoparts
+  module Packages
+    class Elasticbeanstalk < Package
+      name 'elasticbeanstalk'
+      version '2.6.2'
+
+      description "Elastic Beanstalk Command Line Tool: A command line client for interacting with the AWS Elastic Beanstalk APIs"
+
+      category Category::UTILITIES
+
+      source_url 'https://s3.amazonaws.com/elasticbeanstalk/cli/AWS-ElasticBeanstalk-CLI-2.6.2.zip'
+
+      source_sha1 '35e91aed51cc3d39cffe5e557de05d403f30edfc'
+
+      source_filetype 'zip'
+
+      depends_on 'python2'
+      
+      def install
+        prefix_path.parent.mkpath
+        FileUtils.rm_rf prefix_path
+        execute 'mv', extracted_archive_path + 'AWS-ElasticBeanstalk-CLI-2.6.2/eb/linux/python2.7', prefix_path
+      end
+      
+      def required_env
+        [
+          "export PATH=$PATH:#{prefix_path}"
+        ]
+      end
+      
+      def tips
+        <<-STR.unindent
+          Autoparts has added eb to the PATH environment variable for you.
+          To activate eb, please restart your shell session.
+        STR
+      end
+        
+    end
+  end
+end
