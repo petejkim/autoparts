a233e02598b692d5252eadbb2663e8c9b89819c9
Ronoaldo JLP <ronoaldo@gmail.com>
2014-06-01 01:58:16 +0000

    Updates Google App Engine SDKs to 1.9.5
    Updates Google App Engine SDKs for Python/PHP and Go
    to version 1.9.5

--- lib/autoparts/packages/googleappenginego.rb
+++ lib/autoparts/packages/googleappenginego.rb
@@ -2,12 +2,12 @@ module Autoparts
   module Packages
     class GoogleAppEngineGo < Package
       name 'googleappenginego'
-      version '1.9.4'
+      version '1.9.5'
       description 'Google App Engine Go: A CLI for managing Google App Engine cloud services for Go'
       category Category::DEPLOYMENT
 
-      source_url 'https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.4.zip'
-      source_sha1 'ff7c6565d04c99c40fa8fddd0cc8f3ab2e86ba4e'
+      source_url 'https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.5.zip'
+      source_sha1 'cba2407efffd9540d5f3c56460f6958bd5647f68'
       source_filetype 'zip'
 
       def install
