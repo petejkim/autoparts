16f97c460a829622d0721230692d27eb595216a1
Lakshan Perera <lakshan@laktek.com>
2014-06-02 02:15:32 +0000

    Added Copyright notice to Google Cloud SDK package.

--- lib/autoparts/packages/googlecloudsdk.rb
+++ lib/autoparts/packages/googlecloudsdk.rb
@@ -1,3 +1,6 @@
+# Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
+# This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
+
 module Autoparts
   module Packages
     class GoogleCloudSDK < Package
@@ -23,7 +26,7 @@ module Autoparts
           ]
         execute "#{prefix_path}/install.sh", *args
       end
-      
+
       def symlink_all
         # Avoid linking as we added the tools to PATH
       end
