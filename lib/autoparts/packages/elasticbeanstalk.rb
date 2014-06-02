61e90eded2884812d0392768efaa4adedd091878
Lakshan Perera <lakshan@laktek.com>
2014-06-02 02:32:42 +0000

    Symlink to `eb` executable from `.parts/bin`.
    This way the tool will be available immediately for use. No need to
    restart shell session.

--- lib/autoparts/packages/elasticbeanstalk.rb
+++ lib/autoparts/packages/elasticbeanstalk.rb
@@ -6,38 +6,24 @@ module Autoparts
     class Elasticbeanstalk < Package
       name 'elasticbeanstalk'
       version '2.6.2'
-
       description "Elastic Beanstalk Command Line Tool: A command line client for interacting with the AWS Elastic Beanstalk APIs"
-
       category Category::UTILITIES
 
       source_url 'https://s3.amazonaws.com/elasticbeanstalk/cli/AWS-ElasticBeanstalk-CLI-2.6.2.zip'
-
       source_sha1 '35e91aed51cc3d39cffe5e557de05d403f30edfc'
-
       source_filetype 'zip'
 
       depends_on 'python2'
-      
+
       def install
         prefix_path.parent.mkpath
         FileUtils.rm_rf prefix_path
         execute 'mv', extracted_archive_path + 'AWS-ElasticBeanstalk-CLI-2.6.2/eb/linux/python2.7', prefix_path
       end
-      
-      def required_env
-        [
-          "export PATH=$PATH:#{prefix_path}"
-        ]
+
+      def symlink_all
+        symlink_recursively(prefix_path, Path.bin, only_executables: true)
       end
-      
-      def tips
-        <<-STR.unindent
-          Autoparts has added eb to the PATH environment variable for you.
-          To activate eb, please restart your shell session.
-        STR
-      end
-        
     end
   end
 end
