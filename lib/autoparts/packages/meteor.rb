9f959bdfdbfd35b8d3c15d0a21f5fa02ed3e2268
Greg McKeever <gmckeever@gmail.com>
2014-08-28 11:37:05 -0700

    Update Meteor to 0.9.0.1

--- lib/autoparts/packages/meteor.rb
+++ lib/autoparts/packages/meteor.rb
@@ -5,12 +5,12 @@ module Autoparts
   module Packages
     class Meteor < Package
       name 'meteor'
-      version '0.8.3'
+      version '0.9.0.1'
       description 'Meteor: A real-time web development platform'
       category Category::WEB_DEVELOPMENT
 
-      source_url 'https://warehouse.meteor.com/bootstrap/0.8.3/meteor-bootstrap-Linux_x86_64.tar.gz'
-      source_sha1 '7c75aa06d42f13cf3c4f7cd61b65b05a8286644c'
+      source_url 'https://d3sqy0vbqsdhku.cloudfront.net/packages-bootstrap/0.9.0.1/meteor-bootstrap-os.linux.x86_64.tar.gz'
+      source_sha1 'ad2c344f78a3641a00546e023aea0efc9bacd672'
       source_filetype 'tar.gz'
 
       def install
