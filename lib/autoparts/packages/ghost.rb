688c0eec6ab94be5a1fb0dfca47a1adf3144cbeb
Alex Martin <martin.alex.t@gmail.com>
2014-10-12 12:25:42 +0000

    Clean up codebase & remove comments.
    A handful of comments and unecessary code blocks were removed.
    This version still will not install via the IDE.

--- lib/autoparts/packages/ghost.rb
+++ lib/autoparts/packages/ghost.rb
@@ -10,17 +10,15 @@ module Autoparts
       source_sha1 '1505a962395eef123e813a8c24052288013b8d81'
       source_filetype 'zip'
 
-      # Dependencies
       depends_on 'nodejs'
 
-      # Installation
       def install
         Dir.chdir(extracted_archive_path) do
           ghost_path.mkpath
           execute "mv * #{ghost_path}"
         end
         Dir.chdir(ghost_path) do
-          execute "npm", "install", "--production"
+          execute "npm install --production"
         end
       end
 
@@ -29,8 +27,6 @@ module Autoparts
           execute "cp config.example.js config.js"
           execute "sed -i \"s/host: '127.0.0.1',/host: '0.0.0.0',/\" config.js"
           execute "sed -i \"s/port: '2368'/port: '4000'/\" config.js"
-          # execute "sed -i \"s/url: 'http:\/\/my-ghost-blog.com',/url: 'http:\/\/#{hostname}:4000',/\" config.js"
-          # execute "sed -i \"s/url: 'http:\/\/localhost:2368',/url: 'http:\/\/#{hostname}:4000',/\" config.js"
           execute "sed -i \"s/my-ghost-blog.com/#{hostname}.use1-2.nitrousbox.com:4000/\" config.js"
           execute "sed -i \"s/localhost:2368/#{hostname}.use1-2.nitrousbox.com:4000/\" config.js"
         end
@@ -50,17 +46,6 @@ module Autoparts
         end
       end
       
-      # This won't work - use Ctrl+C to terminate process.
-      def stop
-        Dir.chdir(ghost_path) do
-          execute 'npm stop'
-        end
-      end
-
-      def running?
-        !!system(ghost_path.to_s, 'status', out: '/dev/null', err: '/dev/null')
-      end
-
       def tips
         <<-EOS.unindent
           You're ready to blog with Ghost!
@@ -68,10 +53,13 @@ module Autoparts
           To start Ghost up, use:
             $parts start ghost
 
-          To terminate your Ghost instance, use Ctrl+C.
+          To terminate your Ghost instance, use Ctrl+C inside your terminal.
 
           By default, Ghost runs on port 4000 and is bound to all interfaces (0.0.0.0).
           These settings can be changed inside your config.js. 
+
+          If this is your first time using ghost, go to "http://your-preview-address.com:4000/ghost"
+          to get your account set up. 
 
           ENJOY!
         EOS
