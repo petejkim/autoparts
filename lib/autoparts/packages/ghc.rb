85455078a5efa274f38627ef3dd73a0f703a7498
Greg McKeever <gmckeever@gmail.com>
2014-12-15 23:52:57 +0000

    Update GHC to 7.8.3

--- lib/autoparts/packages/ghc.rb
+++ lib/autoparts/packages/ghc.rb
@@ -2,16 +2,16 @@ module Autoparts
   module Packages
     class Ghc < Package
       name 'ghc'
-      version '7.8.2'
+      version '7.8.3'
       description 'Glasgow Haskell Compiler: a state-of-the-art, open source, compiler and interactive environment for the functional language Haskell.'
       category Category::PROGRAMMING_LANGUAGES
 
-      source_url 'http://www.haskell.org/ghc/dist/7.8.2/ghc-7.8.2-x86_64-unknown-linux-deb7.tar.bz2'
-      source_sha1 'e4f88c426db37d6009d0eef406957a62ccb031fa'
+      source_url 'http://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-x86_64-unknown-linux-deb7.tar.bz2'
+      source_sha1 '5c7380809da1d009e0d530149b1ebf6131f9f8e8'
       source_filetype 'tar.bz2'
 
       def compile
-        Dir.chdir('ghc-7.8.2') do
+        Dir.chdir('ghc-7.8.3') do
 	  # Annoying workaround to the fact that the nitrous install doesn't
 	  # have libgmp.so, only libgmp.so.10, and the prebuilt ghc wants the
 	  # .so version.
