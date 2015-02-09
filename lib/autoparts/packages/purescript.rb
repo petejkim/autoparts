f004a55558541bb9c79558a4a621b2b4b3ab1315
Chris Schneider <chris@christopher-schneider.com>
2015-02-09 16:33:55 +0000

    Bump purescript to 0.6.6

--- lib/autoparts/packages/purescript.rb
+++ lib/autoparts/packages/purescript.rb
@@ -5,11 +5,11 @@ module Autoparts
   module Packages
     class Purescript < Package
       name 'purescript'
-      version '0.6.5'
+      version '0.6.6'
       description 'PureScript is a small strongly, statically typed programming language with expressive types, written in and inspired by Haskell, and compiling to Javascript'
       category Category::PROGRAMMING_LANGUAGES
-      source_url 'https://github.com/purescript/purescript/releases/download/v0.6.5/linux64.tar.gz'
-      source_sha1 'f528932c11b724b704897528d81f634a74f5cb0a'
+      source_url 'https://github.com/purescript/purescript/releases/download/v0.6.6/linux64.tar.gz'
+      source_sha1 'c56b1ea11dd00c502220d585e9cb970e1f531b4e'
       source_filetype 'tar.gz'
 
       def install
