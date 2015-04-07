b162ed1ff2f9635a3cd595524b00350d169a2eda
Greg McKeever <gmckeever@gmail.com>
2015-04-07 18:02:24 +0000

    Update Go from 1.4 to 1.4.2

--- lib/autoparts/packages/go.rb
+++ lib/autoparts/packages/go.rb
@@ -5,12 +5,12 @@ module Autoparts
   module Packages
     class Go < Package
       name 'go'
-      version '1.4'
+      version '1.4.2'
       description 'Go: An open source programming language that makes it easy to build simple, reliable, and efficient software.'
       category Category::PROGRAMMING_LANGUAGES
 
-      source_url 'https://storage.googleapis.com/golang/go1.4.linux-amd64.tar.gz'
-      source_sha1 'cd82abcb0734f82f7cf2d576c9528cebdafac4c6'
+      source_url 'https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz'
+      source_sha1 '5020af94b52b65cc9b6f11d50a67e4bae07b0aff'
       source_filetype 'tar.gz'
 
       def install
